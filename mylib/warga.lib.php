c4dfbcd967ed9e11ab30d06ea8c039fa6cd1add9
dimashadii <68457260+dimashadii@users.noreply.github.com>
2021-08-23 10:42:44 +0700

    Add files via upload

---
+++ mylib/warga.lib.php
@@ -0,0 +1,34 @@
+<?php
+class Warga
+{
+    //variabel global
+    var $nama;
+    var $alamat;
+    //tahap 1
+    public function personAwal()
+    {
+        echo $nama_lengkap = "Dudung";
+        echo "<br>";
+        echo $alamat = "Cibadak";
+    }
+    //tahap 2
+    public function person()
+    {
+        echo $nama_lengkap = $this->nama;
+        echo "<br>";
+        echo $alamat = $this->alamat;
+    }
+    public function setPerson()
+    {
+        //menggunakan variabel global
+        $this->nama = "Nama 1";
+        echo "<br>";
+        $this->alamat = "Alamat 1";
+    }
+    public function person3($nama, $alamat)
+    {
+        echo $nama_lengkap = $nama;
+        echo "<br>";
+        echo $alamat = $alamat;
+    }
+}
